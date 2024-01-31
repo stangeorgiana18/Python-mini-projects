5b1516267291ad7fe9e8d01fc49e841950c6789b
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-31 14:45:00 +0000

    first try

--- 2. Calculator.py
+++ 2. Calculator.py
@@ -1,2 +1,56 @@
 # A simple calculator to do basic operators. Make it a scientific calculator for added complexity.
 
+
+# operations
+
+def add(a, b):
+    return a + b
+
+def substract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+
+def calculator():
+
+    # starting value
+    result = 0
+
+    while True:
+
+        try:
+
+            number = int(input("Number: "))
+            print(f"Result: {result}")
+            operator = input("Operator +, -, /, *, c, x: ")
+            if operator == '+':
+                result +=  number
+
+            elif operator == '-':
+                result -= number
+
+            elif operator == '*':
+                result *= number
+
+            elif operator == '/':
+                if number != 0:  # check for division by zero
+                    result /= number
+                else:
+                    print("Cannot divide by zero.")
+
+            elif operator == 'c':
+                # reset the starting value
+                result = 0
+
+            elif operator == 'x':
+                break
+
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    
+calculator()
