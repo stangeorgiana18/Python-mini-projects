594fb71cb5a84df2be36b72602ee1935b0587979
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-31 14:58:50 +0000

    errors fixed

--- 2. Calculator.py
+++ 2. Calculator.py
@@ -1,21 +1,6 @@
 # A simple calculator to do basic operators. Make it a scientific calculator for added complexity.
 
 
-# operations
-
-def add(a, b):
-    return a + b
-
-def substract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    return a / b
-
-
 def calculator():
 
     # starting value
@@ -25,32 +10,40 @@ def calculator():
 
         try:
 
-            number = int(input("Number: "))
-            print(f"Result: {result}")
+            previous_number = result 
+
             operator = input("Operator +, -, /, *, c, x: ")
-            if operator == '+':
-                result +=  number
 
-            elif operator == '-':
-                result -= number
-
-            elif operator == '*':
-                result *= number
-
-            elif operator == '/':
-                if number != 0:  # check for division by zero
-                    result /= number
-                else:
-                    print("Cannot divide by zero.")
-
-            elif operator == 'c':
-                # reset the starting value
-                result = 0
-
-            elif operator == 'x':
+            # break the loop when 'x' is entered
+            if operator == 'x':
                 break
+
+            if operator == 'c':
+                result = 0
+            else:
+
+                number = int(input("Number: "))
+
+                if operator == '+':
+                    result += number
+
+                elif operator == '-':
+                    result -= number
+
+                elif operator == '*':
+                    result *= number
+
+                elif operator == '/':
+                    if number != 0:  # check for division by zero
+                        result /= number
+                    else:
+                        print("Cannot divide by zero.")
+
+                print(f"{previous_number} {operator} {number} = {result}")
+                print('\n')
 
         except ValueError:
             print("Invalid input. Please enter a valid number.")
+
     
 calculator()
